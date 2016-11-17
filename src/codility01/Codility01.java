067e04c8fe2f58cb6d9dfdf50c9c64cde0b8365f
philip.zhou <philip.zhou@asiaplantationcapital.com>
2016-11-17 18:04:52 +0800

    update lesson 7 with nesting, time complexity very bad; update git;

--- src/codility01/Codility01.java
+++ src/codility01/Codility01.java
@@ -15,7 +15,7 @@ public class Codility01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new lesson7().runFish();
+        new lesson7().runNesting();
     }
     
 }
--- src/codility01/lesson7.java
+++ src/codility01/lesson7.java
@@ -21,8 +21,8 @@ public class lesson7 {
 
         int[] B5 = {1, 1, 0, 0, 1}; //3
         System.out.println(Fish(A, B5));
-    }
-
+    }    
+    
     public int Fish(int[] A, int[] B) {
 
         int length = A.length;
@@ -67,5 +67,38 @@ public class lesson7 {
         }
 
         return counter;
+    }
+
+    public void runNesting() {
+        String S1 = "(()(())())()()";
+        String S2 = "())))))))))";
+        System.out.println(Nesting(S2));
+    }
+    
+    public int Nesting(String S) {
+        int length = S.length();
+
+        if(length == 0)
+        {
+            return 1;
+        }
+        
+        char c1 = '(';
+        char c2 = ')';
+
+        int idx = 0;
+
+        idx = S.indexOf(c2);
+        if (idx <= 0) {
+            return 0;
+        }
+
+        if (S.charAt(idx - 1) != c1) {
+            return 0;
+        }
+
+        S = S.substring(0, idx-1) + S.substring(idx+1, length);
+        
+        return Nesting(S);
     }
 }
