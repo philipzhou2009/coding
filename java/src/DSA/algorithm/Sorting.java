b142ca382ab5a614abf0c30f546e6f827c4ddcee
philip.zhou <philip.zhou.2009@gmail.com>
2017-08-05 22:17:25 +0800

    dsa: maven project for easy unit testing;

--- java/src/DSA/algorithm/Sorting.java
+++ java/src/DSA/algorithm/Sorting.java
@@ -1,13 +1,10 @@
-
 package DSA.algorithm;
 
 public class Sorting {
-    
-    
-    
-    
+
     public static int[] bubbleSort(int[] A) {
-        
+        int length = A.length;
+
         return new int[3];
     }
 }
