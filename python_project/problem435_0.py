c201c6f358d3047323b25e09bde57ca6c4a70067
philip.zhou.2009 <philip.zhou.2009@gmail.com>
2020-08-07 23:44:53 +0800

    works for most cases I think but no effcient enough

--- python_project/problem435_0.py
+++ python_project/problem435_0.py
@@ -2,60 +2,80 @@
 from typing import List
 
 
-def intervalSort(e):
-    return e[0]
+def emptyFunc(*args):
+    pass
+
+
+logger = print if True else emptyFunc
+counter = 0
 
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        totalLen = len(intervals)
+        if totalLen == 0:
+            return 0
 
         intervals.sort(key=intervalSort)
-        print(intervals)
+        logger("sorted intervals:", intervals)
+        logger("len:", totalLen)
+        # return 1
 
-        resultList = []
+        resultLen = newFunc(intervals)
+        logger("resultLen", resultLen)
 
-        skips = []
-        skipsLen = 0
+        eraseLen = totalLen - resultLen
+        logger("eraseLen", eraseLen)
+        return eraseLen
 
-        counter = 0
-        while True:
-            lastOne = firstOne = intervals[0]
-            result = [firstOne]
-            i = 0
-            flag = False
-            for interval in intervals[1:]:
-                i += 1
 
-                print("interval", interval)
-                try:
-                    skips.index(i)
-                    print("skip", i)
-                    continue
-                except:
-                    pass     
+def intervalSort(e):
+    return e[0]
 
-                # overlap
-                if interval[0] >= lastOne[1]:
-                    result.append(interval)
-                    lastOne = interval
-                elif flag == False:
-                    skips.append(i-1)
-                    flag = True
 
-            print("skips", skips)        
-            print("result", result)
-            print("===============")
-            
-            newLen = len(skips)
-            if newLen == skipsLen:
-                break
-            else:
-                skipsLen = newLen
-            counter += 1
-            if counter == 5:
-                break
-            # break
+def newFunc(intervals: List[List[int]]):
+    # global counter
+    # counter += 1
+    # if counter > 10:
+    #     return 0
 
-        # print(resultList)
+    lastOne = intervals[0]
+    i = 0
+    flag = False
 
-        return 1
+    for ce in intervals[1:]:
+        i += 1
+        leftValue = ce[0]
+        rightValue = ce[1]
+        if lastOne[1] <= leftValue:
+            lastOne = ce
+            continue
+        else:
+            # overlap
+            flag = True
+            break
+        pass
+
+    if flag == True:
+        list1 = []
+        list1.extend(intervals[0: i])
+        list1.extend(intervals[i+1:])
+        list2 = []
+        list2.extend(intervals[: i-1])
+        list2.extend(intervals[i:])
+
+        logger("list1", list1)
+        logger("list2", list2)
+
+        lenList1 = newFunc(list1)
+        lenList2 = newFunc(list2)
+
+        logger("found length of list1", lenList1)
+        logger("found length of list2", lenList2)
+
+        return lenList1 if lenList1 > lenList2 else lenList2
+        # return 0
+    else:
+        theLen = len(intervals)
+        logger("found a length", theLen)
+        return theLen
