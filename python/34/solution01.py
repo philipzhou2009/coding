0e1f38c23d62f6c7079c8d2dd222e9f202bf8816
philip_zhou <philip_zhou@singaporeair.com.sg>
2020-08-20 21:28:47 +0800

    add more problems

---
+++ python/34/solution01.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+from typing import List
+
+logger = print if True else lambda *arg: None
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        
+
+
+        pass
---
+++ python/486/solution01.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/predict-the-winner/
+
+from typing import List
+
+logger = print if True else lambda *arg: None
+
+class Solution:
+    def func(self, n: int) -> int:
+        pass
