255426551cc63c103a1cff02006c03b6e8d1b5e7
philip.zhou.2009 <philip.zhou.2009@gmail.com>
2020-08-20 23:09:38 +0800

    solve problem 34

--- python/34/solution01.py
+++ python/34/solution01.py
@@ -2,12 +2,55 @@
 
 from typing import List
 
-logger = print if True else lambda *arg: None
+logger = print if False else lambda *arg: None
+
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        
 
-
+        result = func(nums, target, 0)
+        logger("result=%s" % result)
+        return result
         pass
+
+
+def func(nums: List[int], target: int, travel: int) -> List[int]:
+
+    nLen = len(nums)
+
+    if nLen == 0:
+        return [-1, -1]
+
+    if nLen == 1:
+        if nums[0] == target:
+            return [0 + travel, 0 + travel]
+        else:
+            return [-1, -1]
+
+    nHalf = int(nLen / 2)
+
+    logger("nHalf=%s" % nHalf)
+
+    if nums[nHalf - 1] > target:
+        return func(nums[0:nHalf], target, travel)
+    elif nums[nHalf - 1] < target:
+        return func(nums[nHalf:], target, travel + nHalf)
+    else:
+        # found it
+        cursorLeft = cursorRight = nHalf - 1
+        while cursorLeft > -1:
+            if nums[cursorLeft] == target:
+                cursorLeft -= 1
+            else:
+                break
+
+        while cursorRight < nLen:
+            if nums[cursorRight] == target:
+                cursorRight += 1
+            else:
+                break
+
+        logger("left=%s,right=%s,travel=%s" % (cursorLeft + 1, cursorRight - 1, travel))
+        return [cursorLeft + travel + 1, cursorRight + travel - 1]
+
+    pass
--- python/34/solutionTest.py
+++ python/34/solutionTest.py
@@ -8,15 +8,22 @@ solution = Solution()
 
 class Test(unittest.TestCase):
     def test(self):
-        subjects = []
+        subjects = [
+            ([5, 7, 7, 8, 8, 10], 8, [3, 4]),
+            ([5, 7, 7, 8, 8, 10], 6, [-1, -1]),
+            ([], 0, [-1, -1]),
+            ([2, 2], 2, [0, 1]),
+            ([1, 3], 1, [0, 0]),
+            ([1, 4], 4, [1, 1]),
+        ]
 
         if testCase == -1:
             for e in subjects:
-                self.assertEqual(solution.func(e[0]), e[1])
+                self.assertEqual(solution.searchRange(e[0], e[1]), e[2])
                 print("==================================")
         else:
             e = subjects[testCase]
-            self.assertEqual(solution.func(e[0]), e[1])
+            self.assertEqual(solution.searchRange(e[0], e[1]), e[2])
             print("==================================")
 
 
